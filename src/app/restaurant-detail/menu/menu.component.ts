6ce5ecf077b1dd110b4da78b2003f7b8105ec9a9
Carlos Santana <chenrique@indracompany.com>
2018-05-21 21:20:25 -0300

    Adição de detail dos restaurants e review/menu

---
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -0,0 +1,20 @@
+import { ActivatedRoute } from '@angular/router';
+import { RestaurantsService } from './../../restaurants/restaurants.service';
+import { Component, OnInit } from '@angular/core';
+import { Restaurant } from '../../restaurants/restaurant/restaurant.model';
+
+@Component({
+  selector: 'mt-menu',
+  templateUrl: './menu.component.html'
+})
+export class MenuComponent implements OnInit {
+
+  restaurant: Restaurant;
+
+  constructor(private restaurantsService: RestaurantsService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+      this.restaurantsService.restaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurant => this.restaurant = restaurant);
+  }
+}
